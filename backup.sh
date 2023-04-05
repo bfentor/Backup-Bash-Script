bdb9a70cf36533d88e6a6c3ef6b0873b7cc1c28f
bfentor <fentorb24@gmail.com>
2023-04-05 12:49:15 +0200

    Minimum features work

--- backup.sh
+++ backup.sh
@@ -3,30 +3,29 @@
 # Made by Balazs Fentor
 # Simple backup script that saves /home/USER to a compressed file
 
-size=$(sudo du -sh $dir)
-dir=/Users/balika/Documents/Scripting/Mac_Address_Randomizer/
+size=$(sudo du -sh ${backDir}backup$date.tgz)
+dir=/Users/$USER
 backDir=/Users/
 #backDir=/Users/
-
 
 clear
 
 date=$(date +'_%S_%M_%H_%m_%d_%Y')
 
-bold=$(tput bold)
-normal=$(tput sgr0)
+#bold=$(tput bold)
+#normal=$(tput sgr0)
 
 sudo printf ""
 
-echo "${bold}macOS Backup to Archive Utility"
+echo "macOS Backup to Archive Utility"
 echo "© 2023 Balazs Fentor, All rights reserved"
 
 echo -e
 
-#sudo tar -czf /Users/backup$date.tgz /Users/$USER
+sudo tar -czf ${backDir}backup$date.tgz /Users/$USER
 
 # test tar
-sudo tar -czf ${backDir}backup$date.tgz $dir
+#sudo tar -czfv ${backDir}backup$date.tgz $dir
 
 echo -e
 echo "Backup Info:"
@@ -34,6 +33,3 @@ echo -e
 
 echo "Name: ${backDir}backup$date.tgz"
 echo "Size: ${size}"
-
-#echo "Name: /Users/$date"
-#echo "Size: $(sudo du -sh /Users/$date)"
